a2532d90c71c12d5e1550d80134ecf2c60901331
b-pl <bartoszrydzewski@outlook.com>
2021-04-16 11:06:24 +0200

    feat(desktop): dropdown styles & colors

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const toggleDropdown = () => {
   const form = document.querySelector('.form__productVariant')
-  const aaa = document.querySelector('.customSelect--default')
   form.addEventListener('click', () => {
     document.querySelector('.customSelect__options').classList.toggle('customSelect__options--visible')
     document.querySelector('.customSelect--default').classList.toggle('customSelect--default--border')
-    // console.log('clicked')
+    document.querySelector('.customSelect__arrow').classList.toggle('customSelect__arrow--rotate')
   })
 }
 
